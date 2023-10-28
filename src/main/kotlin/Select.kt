3447f7c10b91a50925470db64055f63d149bbb2c
asieralbalate <alualb1634@ieselcaminas.org>
2023-10-28 18:55:38 +0200

    falta el else del select

--- src/main/kotlin/Select.kt
+++ src/main/kotlin/Select.kt
@@ -14,37 +14,71 @@ fun select() {
         println("Introduce los campos a filtrar separados por comas si quieres ver todo introduce *")
         val input = Scanner(System.`in`).nextLine()
 
-        val sentenciaSQL: String
-        if (input == "*") {
-            sentenciaSQL = "SELECT * FROM USUARIO"
+
+        if (input.equals('*')) {
+            val sentenciaSQL = "SELECT * FROM USUARIO"
+            val rs = st.executeQuery(sentenciaSQL)
+            val preparedStatement = con.prepareStatement(sentenciaSQL)
+            System.out.println("ID. \tNombre \tUser \tPassword \tTelefono \tEmail")
+            System.out.println("---------------------------------------------------------")
+            preparedStatement.executeQuery(sentenciaSQL)
+            var rowCount = 0
+            while (rs.next()) {
+                print("" + rs.getInt(1) + "\t")
+                print("" + rs.getString(2) + "\t")
+                print("" + rs.getString(3) + "\t")
+                print("" + rs.getString(4) + "\t")
+                print("" + rs.getDouble(5) + "\t")
+                println("" + rs.getString(6) + "\t")
+                rowCount++
+            }
+
+            println("Número de registros: $rowCount")
+            preparedStatement.close()
+            rs.close()
         } else {
-            val items = input.split(",")
-            val selectColumns = items.joinToString(", ") { it.trim() }
-            sentenciaSQL = "SELECT $selectColumns FROM USUARIO"
+            val niceInput = if (input.endsWith(",")) input.removeSuffix(",") else input
+            val sentenciaSQL = "SELECT $niceInput FROM USUARIO"
+            val items = niceInput.split(",")
+            val rs = st.executeQuery(sentenciaSQL)
+
+            val metaData = rs.metaData
+            val columnCount = metaData.columnCount
+
+
+            var header = ""
+            for (i in 1..columnCount) {
+                header += metaData.getColumnName(i) + "\t"
+            }
+            println("")
+            println(header)
+
+            var rowCount = 0
+            while (rs.next()) {
+                for (i in 1..columnCount) {
+                    val columnName = metaData.getColumnName(i)
+                    val columnType = metaData.getColumnType(i)
+
+                    if (columnType == java.sql.Types.INTEGER) {
+                        print("" + rs.getInt(i) + "\t")
+                    } else if (columnType == java.sql.Types.DOUBLE) {
+                        print("" + rs.getDouble(i) + "\t")
+                    } else {
+                        print("" + rs.getString(i) + "\t")
+                    }
+                }
+                println("")
+                rowCount++
+            }
+            println("Número de registros: $rowCount")
+            println("")
+            rs.close()
         }
 
-        val preparedStatement: PreparedStatement = con.prepareStatement(sentenciaSQL)
-        val rs: ResultSet = preparedStatement.executeQuery()
-
-        System.out.println("ID \tNombre \tUser \tPassword \tTelefono \tEmail \t")
-        System.out.println("---------------------------------------------------------")
-        var rowCount = 0
-        while (rs.next()) {
-            print("" + rs.getInt(1) + "\t")
-            print("" + rs.getString(2) + "\t")
-            print("" + rs.getString(3) + "\t")
-            print("" + rs.getString(4) + "\t")
-            print("" + rs.getInt(5) + "\t")
-            println("" + rs.getString(6) + "\t")
-            rowCount++
-        }
-        println("Número de registros: $rowCount")
-        rs.close()
-        preparedStatement.close()
         st.close()
         con.close()
     } catch (e: SQLException) {
-        println("No existe aún la tabla")
+        println("No existe aun la tabla")
         showMenu()
     }
 }
